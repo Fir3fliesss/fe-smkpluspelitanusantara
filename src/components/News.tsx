94e160c2798866ee4dc72706953280d882de6da8
adityakurnias <adityakurniasaputra903@gmail.com>
2025-01-16 17:43:10 +0700

    Fix news hilang

--- src/components/News.tsx
+++ src/components/News.tsx
@@ -3,10 +3,9 @@ import React, { useEffect, useId, useRef, useState } from "react";
 import { AnimatePresence, motion } from "framer-motion";
 import { useOutsideClick } from "@/hooks/use-outside-click";
 // import { useQuery } from '@tanstack/react-query';
-import BannerText from './BannerText';
-import Button from './Button';
-import jurusan from '../assets/images/informasi_bidang_keahlian.webp';
-
+import BannerText from "./BannerText";
+import Button from "./Button";
+import jurusan from "../assets/images/informasi_bidang_keahlian.webp";
 
 const News: React.FC = () => {
   const [active, setActive] = useState<(typeof cards)[number] | boolean | null>(
@@ -137,40 +136,40 @@ const News: React.FC = () => {
       <ul className="max-w-2xl mx-auto w-full grid grid-cols-1 md:grid-cols-2 items-start gap-4">
         {cards.map((card) => (
           <li key={card.title}>
-              <motion.div
-                layoutId={`card-${card.title}-${id}`}
-                key={card.title}
-                onClick={() => setActive(card)}
-                className="p-4 flex flex-col  hover:bg-neutral-50 dark:hover:bg-neutral-800 rounded-xl cursor-pointer"
-              >
-                <div className="flex gap-4 flex-col  w-full">
-                  <motion.div layoutId={`image-${card.title}-${id}`}>
-                    <img
-                      loading="lazy"
-                      decoding="async"
-                      width={100}
-                      height={100}
-                      src={card.src}
-                      alt={card.title}
-                      className="h-60 w-full  rounded-lg object-cover object-top"
-                    />
-                  </motion.div>
-                  <div className="flex justify-center items-center flex-col">
-                    <motion.h3
-                      layoutId={`title-${card.title}-${id}`}
-                      className="font-medium text-neutral-800 dark:text-neutral-200 text-center md:text-left text-base"
-                    >
-                      {card.title}
-                    </motion.h3>
-                    <motion.p
-                      layoutId={`description-${card.description}-${id}`}
-                      className="text-neutral-600 dark:text-neutral-400 text-center md:text-left text-base"
-                    >
-                      {card.description}
-                    </motion.p>
-                  </div>
+            <motion.div
+              layoutId={`card-${card.title}-${id}`}
+              key={`card-${card.title}-${id}`}
+              onClick={() => setActive(card)}
+              className="p-4 flex flex-col  hover:bg-neutral-50 dark:hover:bg-neutral-800 rounded-xl cursor-pointer"
+            >
+              <div className="flex gap-4 flex-col  w-full">
+                <motion.div layoutId={`image-${card.title}-${id}`}>
+                  <img
+                    loading="lazy"
+                    decoding="async"
+                    width={100}
+                    height={100}
+                    src={card.src}
+                    alt={card.title}
+                    className="h-60 w-full  rounded-lg object-cover object-top"
+                  />
+                </motion.div>
+                <div className="flex justify-center items-center flex-col">
+                  <motion.h3
+                    layoutId={`title-${card.title}-${id}`}
+                    className="font-medium text-neutral-800 dark:text-neutral-200 text-center md:text-left text-base"
+                  >
+                    {card.title}
+                  </motion.h3>
+                  <motion.p
+                    layoutId={`description-${card.description}-${id}`}
+                    className="text-neutral-600 dark:text-neutral-400 text-center md:text-left text-base"
+                  >
+                    {card.description}
+                  </motion.p>
                 </div>
-              </motion.div>
+              </div>
+            </motion.div>
           </li>
         ))}
       </ul>
@@ -179,7 +178,7 @@ const News: React.FC = () => {
       </div>
     </>
   );
-}
+};
 
 export default News;
 
@@ -218,62 +217,85 @@ export const CloseIcon = () => {
 
 const cards = [
   {
-    description: "lorem ipsum",
-    title: "lorem ipsum",
+    description: "lorem ",
+    title: "News 1",
     src: jurusan,
     ctaText: "Baca lebih detail",
     ctaLink: "smkpluspnb.sch.id",
     content: () => {
       return (
         <p>
-          Lorem ipsum dolor, sit amet consectetur adipisicing elit. Adipisci, quo consequatur accusantium aspernatur excepturi cumque nostrum voluptatum possimus dicta deleniti delectus suscipit dolores provident accusamus, quidem ut enim est vero.
-          Tempora ipsa sint aliquid. Iure accusantium magni dolores deserunt debitis. Porro perspiciatis soluta quas, voluptatem doloribus beatae repudiandae fugit quaerat at dolorem, explicabo rerum cumque quasi aspernatur illo. Non, laborum.
+          Lorem ipsum dolor, sit amet consectetur adipisicing elit. Adipisci,
+          quo consequatur accusantium aspernatur excepturi cumque nostrum
+          voluptatum possimus dicta deleniti delectus suscipit dolores provident
+          accusamus, quidem ut enim est vero. Tempora ipsa sint aliquid. Iure
+          accusantium magni dolores deserunt debitis. Porro perspiciatis soluta
+          Haloooooooooooooooooooooooo
+          quas, voluptatem doloribus beatae repudiandae fugit quaerat at
+          dolorem, explicabo rerum cumque quasi aspernatur illo. Non, laborum.
         </p>
       );
     },
   },
   {
-    description: "lorem ipsum",
-    title: "lorem ipsum",
+    description: "lorem ipsum dolor",
+    title: "News 2",
     src: jurusan,
     ctaText: "Baca lebih detail",
     ctaLink: "smkpluspnb.sch.id",
     content: () => {
       return (
         <p>
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsum, facilis corporis. Expedita reiciendis iure a, perspiciatis exercitationem adipisci illum officiis, ipsum distinctio culpa laborum dicta porro praesentium, debitis eius sequi.
-          Libero distinctio tempore accusantium, architecto commodi nemo nulla minus iusto et earum similique beatae recusandae suscipit dicta doloremque, placeat illo eaque natus consequuntur fugiat laborum quisquam vero nihil nostrum. Aliquid!
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsum,
+          facilis corporis. Expedita reiciendis iure a, perspiciatis
+          exercitationem adipisci illum officiis, ipsum distinctio culpa laborum
+          dicta porro praesentium, debitis eius sequi. Libero distinctio tempore
+          accusantium, architecto commodi nemo nulla minus iusto et earum
+          similique beatae recusandae suscipit dicta doloremque, placeat illo
+          eaque natus consequuntur fugiat laborum quisquam vero nihil nostrum.
+          Aliquid!
         </p>
       );
     },
   },
 
   {
-    description: "lorem ipsum",
-    title: "lorem ipsum",
+    description: "lorem ipsum, lorem ipsum",
+    title: "News 3",
     src: jurusan,
     ctaText: "Baca lebih detail",
     ctaLink: "smkpluspnb.sch.id",
     content: () => {
       return (
         <p>
-          Lorem ipsum dolor, sit amet consectetur adipisicing elit. Numquam ipsum nulla porro iusto animi quibusdam ipsa, magnam odio consequuntur! Alias officia pariatur eaque quo rerum modi deserunt exercitationem sequi maxime?
-          Deleniti excepturi veritatis incidunt dolorem ut vel perferendis impedit odit consectetur harum quis est enim inventore, eos quidem tempora nemo totam nam ab quod nobis dolor. Impedit debitis enim aut!
+          Lorem ipsum dolor, sit amet consectetur adipisicing elit. Numquam
+          ipsum nulla porro iusto animi quibusdam ipsa, magnam odio
+          consequuntur! Alias officia pariatur eaque quo rerum modi deserunt
+          exercitationem sequi maxime? Deleniti excepturi veritatis incidunt
+          dolorem ut vel perferendis impedit odit consectetur harum quis est
+          enim inventore, eos quidem tempora nemo totam nam ab quod nobis dolor.
+          Impedit debitis enim aut!
         </p>
       );
     },
   },
   {
-    description: "lorem ipsum",
-    title: "lorem ipsum",
+    description: "lorem ipsum dolor sit amet",
+    title: "News 4",
     src: jurusan,
     ctaText: "Baca lebih detail",
     ctaLink: "smkpluspnb.sch.id",
     content: () => {
       return (
         <p>
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptas consequatur perspiciatis commodi necessitatibus! Asperiores sint voluptate placeat iusto quis, officia quia recusandae perferendis autem debitis expedita illum saepe voluptatibus qui.
-          Deleniti minus voluptatibus totam labore voluptate at nostrum. Facilis architecto sit nemo enim perspiciatis sunt blanditiis consequatur sed, eaque adipisci, explicabo consequuntur veniam accusantium? Deleniti dolor esse aut repellat nostrum!
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptas
+          consequatur perspiciatis commodi necessitatibus! Asperiores sint
+          voluptate placeat iusto quis, officia quia recusandae perferendis
+          autem debitis expedita illum saepe voluptatibus qui. Deleniti minus
+          voluptatibus totam labore voluptate at nostrum. Facilis architecto sit
+          nemo enim perspiciatis sunt blanditiis consequatur sed, eaque
+          adipisci, explicabo consequuntur veniam accusantium? Deleniti dolor
+          esse aut repellat nostrum!
         </p>
       );
     },
