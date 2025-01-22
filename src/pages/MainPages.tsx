753a13ffb337256bc4cbebae2d2c0fe3b23fb1ec
Fir3fliesss <gheraldymoses5@gmail.com>
2025-01-22 18:33:56 +0700

    chore(main page): removing react lazy

--- src/pages/MainPages.tsx
+++ src/pages/MainPages.tsx
@@ -1,21 +1,18 @@
-import React, { Suspense } from 'react';
+import React from 'react';
 
-// Lazy load each component
-const VideoPlayerDash = React.lazy(() => import('@/components/VideoPlayer'));
-const Profile = React.lazy(() => import('@/components/Profile'));
-const BidangKeahlian = React.lazy(() => import('@/components/BidangKeahlian'));
-const Sapras = React.lazy(() => import('@/components/Sapras'));
-const News = React.lazy(() => import('@/components/News'));
-const CallToAction = React.lazy(() => import('@/components/CallToAction'));
-const Gallery = React.lazy(() => import('@/components/Gallery'));
-const SocialMedia = React.lazy(() => import('@/components/SocialMedia'));
-const BannerText = React.lazy(() => import('@/components/BannerText'));
-
-const Loading = () => <div>Loading...</div>; // Fallback UI while loading components
+import VideoPlayerDash from '@/components/VideoPlayer';
+import Profile from '@/components/Profile';
+import BidangKeahlian from '@/components/BidangKeahlian';
+import Sapras from '@/components/Sapras';
+import News from '@/components/News';
+import CallToAction from '@/components/CallToAction';
+import Gallery from '@/components/Gallery';
+import SocialMedia from '@/components/SocialMedia';
+import BannerText from '@/components/BannerText';
 
 const MainPages: React.FC = () => {
   return (
-    <Suspense fallback={<Loading />}>
+    <>
       <section id="video-player">
         <VideoPlayerDash />
       </section>
@@ -46,7 +43,7 @@ const MainPages: React.FC = () => {
       <section id="medsoc">
         <SocialMedia />
       </section>
-    </Suspense>
+    </>
   );
 };
 
